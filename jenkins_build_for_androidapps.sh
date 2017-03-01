0b5d5c4512a9b2bbf668d7cc52b7c7d8b738020b
workonly <workonly@gmail.com>
2017-03-01 10:56:12 +0800

    coding style

--- jenkins_build_for_androidapps.sh
+++ jenkins_build_for_androidapps.sh
@@ -7,23 +7,23 @@ TIMESTAMP=$(date +%Y%m%d.%H%M%S)
 PROJ_NAME=$1
 GIT_BRANCH=$2
 WORK_DIR=`pwd`
-TARGET_PATH=/share/samba/public/release/$PROJ_NAME/$GIT_BRANCH.$TIMESTAMP
+TARGET_PATH=/share/samba/public/release/${PROJ_NAME}/${GIT_BRANCH}.${TIMESTAMP}
 
 # download
-mkdir $TIMESTAMP
-cd $TIMESTAMP
-git clone git@code.moredian.com:terminals/$PROJ_NAME.git
-cd $PROJ_NAME
+mkdir ${TIMESTAMP}
+cd ${TIMESTAMP}
+git clone git@code.moredian.com:terminals/${PROJ_NAME}.git
+cd ${PROJ_NAME}
 
 #compile
-git checkout $GIT_BRANCH
+git checkout ${GIT_BRANCH}
 bash ./gradlew clean build
 
 #cp & cleanup
-mkdir -p $TARGET_PATH
-cp -rf ./app/build/outputs/* $TARGET_PATH
-chmod 777 -R $TARGET_PATH
-rm -rf $WORK_DIR/$TIMESTAMP
+mkdir -p ${TARGET_PATH}
+cp -rf ./app/build/outputs/* ${TARGET_PATH}
+chmod 777 -R ${TARGET_PATH}
+rm -rf ${WORK_DIR}/${TIMESTAMP}
 
 
 
